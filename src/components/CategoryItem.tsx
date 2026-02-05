f5828ed031d7f6c47138e7c010069c0626f326cb
t910935-art <t910935@gmail.com>
2026-02-05 13:43:02 +0100

    boxShadow

--- src/components/CategoryItem.tsx
+++ src/components/CategoryItem.tsx
@@ -1,4 +1,6 @@
 import { Flex } from "@mantine/core";
+import { useHover } from "@mantine/hooks";
+import { Text } from "@mantine/core";
 
 interface CategoryItemProps {
   icon: React.ReactNode;
@@ -8,16 +10,29 @@ interface CategoryItemProps {
 }
 
 const CategoryItem = ({ icon, title, onClick, active }: CategoryItemProps) => {
+  const { hovered, ref } = useHover();
+
   return (
     <Flex
+      ref={ref}
       direction={"column"}
       align={"center"}
       justify={"center"}
       gap={"xs"}
       p={"md"}
-      style={{}}
+      style={{
+        border: "1px solid",
+        borderBlockColor: active ? "blue" : "white",
+        borderRadius: "10px",
+        padding: "10px",
+        transition: "all 0.3s ease",
+        boxShadow: hovered ? "0 0 10px rgba(0, 0, 0, 0.1)" : "none",
+        transform: hovered ? "scale(1.05)" : "scale(1)",
+        cursor: "pointer",
+      }}
     >
-      CategoryItem
+      {icon}
+      <Text>{title} </Text>
     </Flex>
   );
 };
--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,12 +1,20 @@
 import { Text } from "@mantine/core";
 import QuizContainer from "../components/QuizContainer";
 import CategoryItem from "../components/CategoryItem";
+import { BiBrain } from "react-icons/bi";
 
 const CategoryPage = () => {
   return (
     <QuizContainer>
       <Text>Category</Text>
-      <CategoryItem />
+      <CategoryItem
+        icon={<BiBrain />}
+        title={"General Knowledge"}
+        onClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        active
+      />
     </QuizContainer>
   );
 };
