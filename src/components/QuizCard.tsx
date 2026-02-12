0360a4c4d4aaac1d18fd51167cd2c6698efee1d9
t910935-art <t910935@gmail.com>
2026-02-12 13:50:12 +0100

    Routing

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,5 +1,7 @@
-import { Flex } from "@mantine/core";
+import { Button, Flex } from "@mantine/core";
 import { Text } from "@mantine/core";
+import { useState } from "react";
+import { FaA, FaArrowRight } from "react-icons/fa6";
 
 interface QuizCardProps {
   question: string;
@@ -9,12 +11,43 @@ interface QuizCardProps {
   onNext: () => void;
 }
 
+const SAMPLE_QUESTION = ["Option 1", "Option 2", "Option 3", "Option 4"];
+
 const QuizCard = ({}: QuizCardProps) => {
+  const [selectedAnswer, setSelectedAnswer] = useState<string>("");
   return (
     <Flex miw={"90%"} direction={"column"} gap={10}>
       <Text size={"xl"} fw={"500"} mt={10} mb={10}>
-        Question
+        question
       </Text>
+      <Flex direction={"column"} gap={10}>
+        {SAMPLE_QUESTION.map((option, index) => (
+          <Button
+            onClick={() => {
+              setSelectedAnswer(option);
+            }}
+            c={selectedAnswer === option ? "white" : "gray"}
+            key={index}
+            bd={"0.5px solid deeppink"}
+            variant={selectedAnswer === option ? "gradiant" : "outline"}
+            radius={"xl"}
+            size={"lg"}
+          >
+            {option}
+          </Button>
+        ))}
+        <Flex gap={10} justify={"end"} mt={10}>
+          <Button
+            variant="gradiant"
+            gradient={{ from: "red", to: "deeppink" }}
+            radius={"xl"}
+            size={"lg"}
+            rightSection={<FaArrowRight />}
+          >
+            Next
+          </Button>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
