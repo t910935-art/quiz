14351923e3bd0365b586787fa9b830afd71965f7
t910935-art <t910935@gmail.com>
2026-02-13 13:49:33 +0100

    route test

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import MainLayout from "./layout/MainLayout";
 import MantineProvider from "./providers/MantineUIProvider";
 
 import "./styles/globale.css";
 import QuizPage from "./pages/QuizPage";
+import WelcomePage from "./pages/WelcomePage";
+import { Center } from "@mantine/core";
+import CategoryPage from "./pages/CategoryPage";
+import QuizStartPage from "./pages/QuizStartPage";
 //import CategoryPage from "./pages/CategoryPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
       <MainLayout>
-        <QuizPage />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/category" element={<CategoryPage />} />
+            <Route path="/quiz-info" element={<QuizStartPage />} />
+            <Route path="/start-quiz" element={<QuizPage />} />
+          </Routes>
+        </BrowserRouter>
       </MainLayout>
     </MantineProvider>
   </StrictMode>,
--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,10 +1,56 @@
+import { Button, Grid, Text, TextInput } from "@mantine/core";
+import { FaCheckCircle } from "react-icons/fa";
+import { GiBrain, GiTrophy } from "react-icons/gi";
+import { IoFlash } from "react-icons/io5";
+
 import QuizContainer from "../components/QuizContainer";
-import { Text } from "@mantine/core";
 
 const WelcomePage = () => {
   return (
     <QuizContainer>
-      <Text>Welcome to the Quiz App!</Text>
+      <Text
+        ta={"center"}
+        fw={"bold"}
+        variant="gradient"
+        fz={{ base: "2rem", md: "3rem", lg: "2rem" }}
+        gradient={{ from: "red", to: "deeppink", deg: 45 }}
+      >
+        - Welcome to Quizify Pro -
+      </Text>
+      <Text ta={"center"} fz={"1.5rem"} fw={"light"} c={"dimmed"}>
+        Challenge Your Mind, Expand Your Knowledge
+      </Text>
+      {/* <ActionIconBox label="Compete" /> */}
+
+      <Grid miw={"90%"}>
+        <Grid.Col span={{ base: 12, md: 6, lg: 4 }}></Grid.Col>
+        <Grid.Col span={{ base: 12, md: 6, lg: 4 }}></Grid.Col>
+        <Grid.Col span={{ base: 12, md: 6, lg: 4 }}> </Grid.Col>
+      </Grid>
+      <Text ta={"center"} fz={"lg"} maw={"80%"} fw={"light"} c={"dimmed"}>
+        Embark on a journey of knowledge with QuizMaster. Test your skills
+        across various categories, challenge friends, and watch your expertise
+        grow with each quiz you take.
+      </Text>
+
+      <TextInput
+        placeholder="Enter your full name to start"
+        radius={"xl"}
+        w={"300px"}
+        size="lg"
+        rightSection={<FaCheckCircle color="green" />}
+      />
+      <a href="/category">
+        <Button
+          variant="gradiant"
+          miw={"200px"}
+          size="lg"
+          radius={"xl"}
+          gradient={{ from: "red", to: "deeppink" }}
+        >
+          Start Quiz
+        </Button>
+      </a>
     </QuizContainer>
   );
 };
